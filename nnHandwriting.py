b425f18f6cc5e6be03038466414cf4f5d4f4414d
L <l@L.Think>
2018-03-08 19:27:17 +0800

    I mainly made some change on activiate function and optimizer.I can now reach a accuracy of over 98% on the test-set

--- nnHandwriting.py
+++ nnHandwriting.py
@@ -19,17 +19,32 @@ n_batch=mnist.train.num_examples//batch_size
 #定义占位符
 x=tf.placeholder(tf.float32,[None,784])
 y=tf.placeholder(tf.float32,[None,10])
+keep_prob=tf.placeholder(tf.float32)
 
-#创建一个简单的神经网络
-W=tf.Variable(tf.zeros([784,10]))
-b=tf.Variable(tf.zeros([10]))
-prediction=tf.nn.softmax(tf.matmul(x,W)+b)
+#隐层1
+W1=tf.Variable(tf.truncated_normal([784,2000],stddev=0.1))
+b1=tf.Variable(tf.zeros([2000])+0.1)
+L1=tf.nn.tanh(tf.matmul(x,W1)+b1)
+L1_drop=tf.nn.dropout(L1,keep_prob)
+
+#隐层2
+W2=tf.Variable(tf.truncated_normal([2000,1500],stddev=0.1))
+b2=tf.Variable(tf.zeros([1500])+0.1)
+L2=tf.nn.tanh(tf.matmul(L1_drop,W2)+b2)
+L2_drop=tf.nn.dropout(L2,keep_prob)
+
+#输出层
+W3=tf.Variable(tf.truncated_normal([1500,10],stddev=0.1))
+b3=tf.Variable(tf.zeros([10])+0.1)
+prediction=tf.nn.softmax(tf.matmul(L2_drop,W3)+b3)
 
 
-#二次代价函数
-loss=tf.reduce_mean(tf.square(y-prediction))
 
-#使用梯度下降法学习
+
+#代价函数
+loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
+
+#学习
 train_step=tf.train.GradientDescentOptimizer(0.2).minimize(loss)
 
 #初始化变量
@@ -46,7 +61,8 @@ with tf.Session() as sess:
     for epoch in range(21):
         for batch in range (n_batch):
             batch_xs,batch_ys=mnist.train.next_batch(batch_size)
-            sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys})
+            sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:1})
             
-        acc=sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels})
-        print("Iter:"+str(epoch)+"  Accuracy:"+str(acc))
+        test_acc=sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1})
+        #train_acc=sess.run(accuracy,feed_dict={x:mnist.train.images,y:mnist.train.labels,keep_prob:1})        
+        print("Iter:"+str(epoch)+"  Test_Accuracy:"+str(test_acc)+",Train_Accuracy:"+str(train_acc))
